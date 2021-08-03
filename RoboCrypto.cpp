1b6e52ee96e0e2e5b0ded39d9e1e7aa10c0fad49
Ferdinand <Ferdinand@LAPTOP-6ECL73F9>
2021-08-04 00:59:54 +0100

    Add project files.

---
+++ RoboCrypto.cpp
@@ -0,0 +1,12 @@
+﻿// RoboCrypto.cpp : Defines the entry point for the application.
+//
+
+#include "RoboCrypto.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
