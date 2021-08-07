5879e044f260cba7fd19061c168836a8d886e626
ferdinand <ferdinandvanwalree@gmail.com>
2021-08-07 01:55:49 +0100

    Adding binapi as dependency

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,31 @@
 //
 
 #include "bypto/robo_crypto.h"
+#include <binapi/api.hpp>
 
-using namespace std;
+#include <boost/asio/io_context.hpp>
 
-int main()
-{
-    cout << "Hello CMake." << endl;
-    return 0;
+#include <iostream>
+
+int main() {
+    boost::asio::io_context ioctx;
+    binapi::rest::api api{
+         ioctx
+        ,"api.binance.com"
+        ,"443"
+        ,"" // can be empty for non USER_DATA reqs
+        ,"" // can be empty for non USER_DATA reqs
+        ,10000 // recvWindow
+    };
+
+    auto res = api.price("BTCUSDT");
+    if (!res) {
+        std::cerr << "get price error: " << res.errmsg << std::endl;
+
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "price: " << res.v << std::endl;
+
+    return EXIT_SUCCESS;
 }
