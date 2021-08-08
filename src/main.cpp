05b93380be10b360606d3aea91260691fe9a5556
ferdinand <ferdinandvwalree@gmail.com>
2021-08-08 15:17:42 +0100

    Successfully connected to binance test network

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,26 @@
 //
 
 #include "bypto/robo_crypto.h"
-#include <binapi/api.hpp>
 
 #include <boost/asio/io_context.hpp>
+#include <binapi/api.hpp>
 
+#include <cstdlib>
 #include <iostream>
 
 int main() {
-    boost::asio::io_context ioctx;
+    const auto pk = std::getenv("BINANCE_TEST_PUBLIC_KEY");
+    const auto sk = std::getenv("BINANCE_TEST_SECRET_KEY");
+
+    std::cout << "public key: " << pk << std::endl;
+    std::cout << "secret key: " << sk << std::endl;
+    boost::asio::io_context io;
     binapi::rest::api api{
-         ioctx
-        ,"api.binance.com"
+         io
+        ,"testnet.binance.vision"
         ,"443"
-        ,"" // can be empty for non USER_DATA reqs
-        ,"" // can be empty for non USER_DATA reqs
+        ,pk // can be empty for non USER_DATA reqs
+        ,sk // can be empty for non USER_DATA reqs
         ,10000 // recvWindow
     };
 
@@ -27,6 +33,14 @@ int main() {
     }
 
     std::cout << "price: " << res.v << std::endl;
+
+    auto account = api.account_info();
+    if ( !account ) {
+        std::cerr << "account info error: " << account.errmsg << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "account info: " << account.v << std::endl << std::endl;
 
     return EXIT_SUCCESS;
 }
