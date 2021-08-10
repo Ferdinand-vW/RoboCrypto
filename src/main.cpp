c2f8f24fe3f6a3f5393908956b248fd6de20251b
ferdinand <ferdinandvwalree@gmail.com>
2021-08-10 01:01:16 +0100

    Understanding order responses

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 ﻿// RoboCrypto.cpp : Defines the entry point for the application.
 //
 
+#include "binapi/enums.hpp"
 #include "bypto/robo_crypto.h"
 
 #include <boost/asio/io_context.hpp>
@@ -41,6 +42,26 @@ int main() {
     }
 
     std::cout << "account info: " << account.v << std::endl << std::endl;
+
+    auto resp = api.new_order("BTCUSDT",binapi::e_side::buy,binapi::e_type::market,binapi::e_time::FOK,binapi::e_trade_resp_type::FULL
+                             ,"20",nullptr,"myid",nullptr,nullptr);
+
+    std::cout << "new order err " << resp.errmsg << std::endl;
+    std::cout << "new order " << resp.reply << std::endl;
+
+    auto orders = api.all_orders("BTCUSDT");
+    std::cout << "order error" << orders.errmsg << std::endl;
+    std::cout << "orders " << orders.reply << std::endl;
+    for (auto &o : orders.v.orders) {
+        std::cout << o.first << std::endl;
+        for(auto &oi : o.second) {
+            std::cout << oi.clientOrderId << " " << oi.orderId << " " << oi.cummulativeQuoteQty << " " << oi.executedQty;
+            std::cout << " " << oi.icebergQty << " " << oi.isWorking << " " << oi.origQty << " " << oi.price << " ";
+            std::cout << oi.stopPrice << " " << oi.side << " " << oi.status << " " << oi.symbol << " " << oi.time << " ";
+            std::cout << oi.timeInForce << " " << oi.updateTime << " " << oi.type << std::endl;
+        }
+    }
+    
 
     return EXIT_SUCCESS;
 }
